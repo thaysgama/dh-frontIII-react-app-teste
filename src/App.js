b8b3e2471898c5d189508021f8c1fb988983b5ab
thaysgama <thays.s.gama@gmail.com>
2021-10-25 11:46:20 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://assets-global.website-files.com/5e9aa66fd3886aa2b4ec01ca/5fa5462124f6db4a79de1899_webdevelopercoding.gif" className="" alt="" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
